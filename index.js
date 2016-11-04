c1979ad81bbf449b0b22a61e4bb420dfd4b77c88
zhanghao <jsyczhanghao@sina.com>
2016-11-04 13:34:14 +0800

    commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var REG = /<script [^>]*\bscript2bottom[^>]*>[\s\S]*?<\/script>/g;
+
+module.exports = function(ret){
+    feather.util.map(ret.src, function(subpath, file){
+        if(file.isHtmlLike){
+            var content = file.getContent(), stack = [];
+
+            content = content.replace(REG, function(all){
+                stack.push(all);
+                return '';
+            });
+
+            if(!file.isPageletLike && content.indexOf('</body>') > -1){
+                content = content.replace(/<\/body>/i, function(){
+                    return stack.join('') + '</body>';
+                });
+            }else{
+                content += stack.join('');
+            }
+
+            file.setContent(content);
+
+        }
+    });
+};
