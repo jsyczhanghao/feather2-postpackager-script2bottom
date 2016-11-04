3ff7090442f66113b1df8fa97af1cbf5771f9103
jsyczhanghao <jsyczhanghao@sina.com>
2016-11-04 15:43:22 +0800

    commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 
-var REG = /<script [^>]*\bscript2bottom[^>]*>[\s\S]*?<\/script>/g;
+var REG = /<script( [^>]*?\bscript2bottom[^>]*)>([\s\S]*?)<\/script>/g;
 
 module.exports = function(ret){
     feather.util.map(ret.src, function(subpath, file){
         if(file.isHtmlLike){
             var content = file.getContent(), stack = [];
 
-            content = content.replace(REG, function(all){
+            content = content.replace(REG, function(all, _1, _2){
+                all = '<script' + _1.replace(/\s*script2bottom\s*/, ' ').replace(/\s*$/, '') + '>' + _2 + '</script>';
                 stack.push(all);
                 return '';
             });
